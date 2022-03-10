e619503da2ded1ff858ace4da5a2e776ccbbf7d2
swapnil-deotare <swapnildeotare@gmail.com>
2022-03-09 17:39:38 -0800

    React Chat App Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <ChatEngine
+            height = "100vh"
+            projectID = "6fe86520-c142-43c3-8bb8-288fe7ba8d09"
+            userName = "Swapnil"
+            userSecret = "Test12345"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        />
+        
+    )
 }
 
 export default App;
